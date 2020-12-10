cbf617d9f07e9dc2c8a4d768b256f1021ea27f55
Anthony Veaudry <anthony@veaudry.pro>
2020-12-11 01:04:36 +0200

    clean up project
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './react.svg';
-import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
         <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
+          {/* <img src={logo} className="Home-logo" alt="logo" /> */}
           <h2>Welcome to Razzle</h2>
         </div>
         <p className="Home-intro">
